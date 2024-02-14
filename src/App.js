23ba7668e7a0a74a1d870f0d97e8c4787948a55a
SharkTax <103666028+SharkTax@users.noreply.github.com>
2024-02-14 11:05:29 -0300

    random rgb or hex colors (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+
+import RandomColor from './components/random-color';
 import Accordian from './components/accordian';
+
 
 function App() {
   return (
     <div className="App">
       {/*Accordian component*/}
       <Accordian/>
+      {/*Random hex color*/}
+      <RandomColor/>
+
     </div>
   );
 }
