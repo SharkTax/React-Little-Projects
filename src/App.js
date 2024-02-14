fc328b35ff25bae00d5210106611f76d5690a798
Victor Maciel Pinheiro <Promissevm.st@gmail.com>
2024-02-14 10:58:57 -0300

    random rgb or hex colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/*Accordian component*/}
-      <Accordian/>
 
       {/*Random hex color*/}
       <RandomColor/>
