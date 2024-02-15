a96ffa66720c396d8091b7a004991838feb3b331
SharkTax <103666028+SharkTax@users.noreply.github.com>
2024-02-15 00:15:22 -0300

    Update App.js
    updatig to delete another apps not relationated to the star-rating

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from "./components/randomHex"
 import StartRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      {/*Accordian component*/}
-      <Accordian/>
-
-      <RandomColor/>
-
       <StartRating noOfStars={10}/>
     </div>
   );
