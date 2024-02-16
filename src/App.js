5e470831abadcca83cda3b8113ce54b0538fe18d
SharkTax <103666028+SharkTax@users.noreply.github.com>
2024-02-16 17:03:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from "./components/randomHex"
-import StartRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
 
 function App() {
   return (
     <div className="App">
-      {/*Accordian component*/}
-      <Accordian/>
-
-      <RandomColor/>
-
-      <StartRating noOfStars={10}/>
 
       <ImageSlider
         url={"https://picsum.photos/v2/list"}
