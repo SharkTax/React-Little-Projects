614681846abac7a77f3c6c0b0c0fdd2273a1fd00
Victor Maciel Pinheiro <Promissevm.st@gmail.com>
2024-02-16 16:57:35 -0300

    esse è um aplicativo para um slider de imagens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from "./components/randomHex"
 import StartRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -12,6 +13,12 @@ function App() {
       <RandomColor/>
 
       <StartRating noOfStars={10}/>
+
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
