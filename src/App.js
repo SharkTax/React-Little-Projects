89ca05f08f782e591f300b01683a314b23600d93
SharkTax <103666028+SharkTax@users.noreply.github.com>
2024-02-18 22:35:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import './App.css';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-images';
 import Treeview from './components/side-bar';
-import menus from './components/side-bar/list-sidebra';
 
 function App() {
   return (
     <div className="App">
-
-
-      <Accordian/>
-
-      <RandomColor/>
-
-      <StartRating noOfStars={10}/>
-
-      <ImageSlider
-        url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={"10"}
-      />
-
-      <LoadMoreData/> 
       <Treeview menus={menus}/>
     </div>
   );
