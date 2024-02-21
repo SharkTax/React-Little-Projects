df7566011c4aa0d6b5db05068ac0e1d86a6bc17b
SharkTax <103666028+SharkTax@users.noreply.github.com>
2024-02-21 00:18:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from "./components/randomHex"
-import StartRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-images';
-import Treeview from './components/side-bar';
-import menus from './components/side-bar/list-sidebra';
-import QRCodeGenerator from './components/qr-code-generator';
 import LightDarkMode from './components/light-dark-mode';
 
 function App() {
   return (
     <div className="App">
-
-      <Accordian/>
-
-      <RandomColor/>
-
-      <StartRating noOfStars={10}/>
-
-      <ImageSlider
-        url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={"10"}
-      />
-
-      <LoadMoreData/> 
-      <Treeview menus={menus}/>
-
-      <QRCodeGenerator/>
-
       <LightDarkMode/>
     </div>
   );
